19ea3bd914d34d892aeec82f97da5c301521acaf
Mateusz Rzeczyca <mateuszrzeczyca17@gmail.com>
2022-11-08 11:46:16 +0100

    checked log msgs

--- hello_drivers/hello_drivers.c
+++ hello_drivers/hello_drivers.c
@@ -11,7 +11,27 @@ MODULE_VERSION("1:0.1-hellodrivers");
 
 static int __init hello_drivers_init(void)
 {
-    printk(KERN_INFO "Hello Drivers from Mateusz\n");
+    printk(KERN_DEBUG "Debug Hello Drivers from Mateusz\n");
+    printk(KERN_INFO "Info Hello Drivers from Mateusz\n");
+    printk(KERN_NOTICE "Notice Hello Drivers from Mateusz\n");
+    printk(KERN_WARNING "Warning Hello Drivers from Mateusz\n");
+    printk(KERN_ERR "Error Hello Drivers from Mateusz\n");
+    printk(KERN_CRIT "Critical Hello Drivers from Mateusz\n");
+    printk(KERN_ALERT "Alert Hello Drivers from Mateusz\n");
+    printk(KERN_EMERG "Emergency Hello Drivers from Mateusz\n");
+
+    pr_info("Checking pr_info...");
+    pr_cont("Continuing pr_info...");
+
+    pr_debug("Checking pr_debug...");
+    pr_cont("Continuing pr_debug...");
+
+    pr_err("Checking pr_err...");
+    pr_cont("Continuing pr_err...");
+
+    pr_warn("Checking pr_warn...");
+    pr_cont("Continuing pr_warn...");
+
     return 0;
 }
 
