57f300bcbed88db3b3cdd54c6d68ec9bed59df6a
Mateusz Rzeczyca <mateuszrzeczyca17@gmail.com>
2022-11-10 20:56:05 +0100

    done second driver

--- 2_hello_parameters/README.md
+++ 2_hello_parameters/README.md
@@ -4,9 +4,18 @@
 ```bash
 sudo make
 sudo insmod hello_parameters.ko value_int=7 value_arr=17,172,1730,17400 value_name="Mateusz"
+
 dmesg
-lsmod
-modinfo hello_parameters.ko
+cd /sys/module/hello_parameters/parameters
+sudo cat value_arr
+sudo cat value_int
+sudo cat value_name
+
+sudo cat value_int_cb
+sudo sh -c "echo 21 > /sys/module/hello_parameters/parameters/value_int_cb"
+sudo cat value_int_cb
+dmesg
+
 sudo rmmod hello_parameters.ko
 dmesg
 ```
--- 2_hello_parameters/hello_parameters.c
+++ 2_hello_parameters/hello_parameters.c
@@ -6,12 +6,12 @@
  
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Mateusz Rzeczyca");
-MODULE_DESCRIPTION("Hello Drivers!");
-MODULE_VERSION("1:0.2-helloparameters");
+MODULE_DESCRIPTION("Hello Parameters!");
+MODULE_VERSION("1:0.1-helloparameters");
 
-int value_int = 0;
+int value_int;
 int value_arr[4];
-char* value_name = "";
+char* value_name;
 int value_int_cb = 0;
  
 // /sys/module/hello_parameters/parameters/value_int
@@ -33,15 +33,15 @@ module_param_array(value_arr, int, NULL, S_IRUSR | S_IWUSR);
 
 
 // Only cb params can be notified about value change
-int set_notify_param_cb(const char* val, const struct kernel_param* kp)
+int set_notify_param_cb(const char *val, const struct kernel_param *kp)
 {
     int res = param_set_int(val, kp);
     if (res == 0) {
-        printk(KERN_INFO "Callback setter function called in order to notify kernel that value has changed...\n");
-        printk(KERN_INFO "New value of value_int_cb = %d\n", value_int_cb);
+        pr_info("Callback setter function called in order to notify kernel that value has changed...\n");
+        pr_info("New value of value_int_cb = %d\n", value_int_cb);
         return 0;
     }
-    printk(KERN_ERR, "Callback setter function failed! Cannot update value_int_cb!\n");
+    pr_err("Callback setter function failed! Cannot update value_int_cb!\n");
     return -1;
 }
 
@@ -53,26 +53,32 @@ const struct kernel_param_ops my_param_ops =
 };
 
 
-module_param_cb(value_int_cb, &my_param_ops, &value_int_cb, S_IRGRP | S_IWUSR );
+module_param_cb(value_int_cb, &my_param_ops, &value_int_cb, S_IRUGO | S_IWUSR);
+
+
+static void display_arr(int* arr, int size) {
+    int i = 0;
+    for (i = 0; i < size; i++) {
+        pr_info("value_arr[%d] = %d\n", i, arr[i]);
+    }
+}
 
 
 static int __init hello_parameters_init(void)
 {
-    printk(KERN_INFO "value_int = %d  \n", value_int);
-    for (int i = 0; i < (sizeof(value_arr) / sizeof (int)); i++) {
-        printk(KERN_INFO "value_arr[%d] = %d\n", i, value_arr[i]);
-    }
-    printk(KERN_INFO "value_name = %d  \n", value_name);
-    printk(KERN_INFO "value_int_cb = %s \n", value_int_cb);
+    pr_info("value_int = %d \n", value_int);
+    display_arr(value_arr, sizeof(value_arr) / sizeof (int));
+    pr_info("value_name = %s \n", value_name);
+    pr_info("value_int_cb = %d \n", value_int_cb);
 
-    printk(KERN_INFO "Hello Parameters Inserted Successfully...\n");
+    pr_info("Hello Parameters Inserted Successfully...\n");
     return 0;
 }
 
 
 static void __exit hello_parameters_exit(void)
 {
-    printk(KERN_INFO "Hello Parameters saying goodbye...\n");
+    pr_info("Hello Parameters saying goodbye...\n");
 }
 
 
